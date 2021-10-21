15d735f48d76a4ce4ba2b8ea4e44551a06a36764
Vayun Mathur <69178052+vayun-mathur@users.noreply.github.com>
2021-10-21 21:12:59 +0800

    Added NullEncoder for non-existant encoders

---
+++ src/main/java/frc/lib/encoder/NullEncoder.java
@@ -0,0 +1,32 @@
+package frc.lib.encoder;
+
+public class NullEncoder implements Encoder {
+
+    @Override
+    public double getPosition() {
+        return 0;
+    }
+
+    @Override
+    public double getAbsolutePosition() {
+        return 0;
+    }
+
+    @Override
+    public void setPosition(double newPosition) {
+    }
+
+    @Override
+    public double getVelocity() {
+        return 0;
+    }
+
+    @Override
+    public void setPositionConversionFactor(double factor) {
+    }
+
+    @Override
+    public void setVelocityConversionFactor(double factor) {
+    }
+    
+}
