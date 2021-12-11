c9ae9f96c193e88ebbe9c4ed7d45d00ab8b03bd9
Vayun Mathur <69178052+vayun-mathur@users.noreply.github.com>
2021-12-11 18:58:21 +0800

    Added ODN_TalonFX

--- src/main/java/frc/lib/encoder/ODN_TalonEncoder.java
+++ src/main/java/frc/lib/encoder/ODN_TalonEncoder.java
@@ -1,18 +1,26 @@
 package frc.lib.encoder;
 
+import com.ctre.phoenix.motorcontrol.can.BaseTalon;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import frc.lib.motorcontroller.ODN_TalonFX;
 import frc.lib.motorcontroller.ODN_TalonSRX;
 
 public class ODN_TalonEncoder implements Encoder {
 
-    private WPI_TalonSRX m_backend;
+    private BaseTalon m_backend;
     private int m_sensorID;
 
     private double m_positionFactor = 1;
     private double m_velocityFactor = 1;
 
     public ODN_TalonEncoder(ODN_TalonSRX talon, int sensorID) {
+        m_backend = talon.getBackend();
+        m_sensorID = sensorID;
+    }
+
+    public ODN_TalonEncoder(ODN_TalonFX talon, int sensorID) {
         m_backend = talon.getBackend();
         m_sensorID = sensorID;
     }
@@ -49,8 +57,12 @@ public class ODN_TalonEncoder implements Encoder {
         this.m_velocityFactor = factor;
     }
 
-    public WPI_TalonSRX getBackend() {
-        return m_backend;
+    public WPI_TalonSRX getBackendSRX() {
+        return (WPI_TalonSRX)m_backend;
+    }
+
+    public WPI_TalonFX getBackendFX() {
+        return (WPI_TalonFX)m_backend;
     }
     
 }
---
+++ src/main/java/frc/lib/motorcontroller/ODN_TalonFX.java
@@ -0,0 +1,41 @@
+package frc.lib.motorcontroller;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import frc.lib.encoder.Encoder;
+import frc.lib.encoder.ODN_TalonEncoder;
+
+public class ODN_TalonFX implements MotorController {
+
+    private WPI_TalonFX m_backend;
+
+    public ODN_TalonFX(int CAN_ID) {
+        m_backend = new WPI_TalonFX(CAN_ID);
+    }
+
+    @Override
+    public void set(double speed) {
+        m_backend.set(speed);
+    }
+
+    @Override
+    public void setVoltage(double voltage) {
+        m_backend.setVoltage(voltage);
+    }
+
+    @Override
+    public void setInverted(boolean isInverted) {
+        m_backend.setInverted(isInverted);
+    }
+
+    @Override
+    public Encoder getEncoder() {
+        return new ODN_TalonEncoder(this, 0);
+    }
+
+    @Override
+    public WPI_TalonFX getBackend() {
+        return m_backend;
+    }
+    
+}
