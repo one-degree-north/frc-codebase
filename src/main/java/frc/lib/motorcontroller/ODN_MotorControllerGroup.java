7e27f8eeb362a5c7a08835e1e5a6fcd81200cb48
Vayun Mathur <69178052+vayun-mathur@users.noreply.github.com>
2022-01-09 19:25:31 +0800

    lib MotorControllerGroup implement MotorController

--- src/main/java/frc/lib/motorcontroller/ODN_MotorControllerGroup.java
+++ src/main/java/frc/lib/motorcontroller/ODN_MotorControllerGroup.java
@@ -2,8 +2,9 @@ package frc.lib.motorcontroller;
 
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import frc.lib.encoder.ODN_Encoder;
 
-public class ODN_MotorControllerGroup {
+public class ODN_MotorControllerGroup implements ODN_MotorController {
 
     private MotorControllerGroup m_backend;
 
@@ -30,5 +31,10 @@ public class ODN_MotorControllerGroup {
 	public void setVoltage(double outputVolts) {
         m_backend.setVoltage(outputVolts);
 	}
+
+    @Override
+    public ODN_Encoder getEncoder() {
+        return null;
+    }
     
 }
