6ee5a20d422c1be8ae48e3de46117bd13c6560f7
Vayun Mathur <69178052+vayun-mathur@users.noreply.github.com>
2022-01-20 22:22:25 +0800

    adafruit164 ir sensor

--- src/main/java/frc/lib/sensor/ODN_Adafruit164Sensor.java
+++ src/main/java/frc/lib/sensor/ODN_Adafruit164Sensor.java
@@ -3,21 +3,21 @@ package frc.lib.sensor;
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.RobotController;
 
-public class ODN_AnalogUltrasonicSensor implements ODN_DistanceSensor {
+public class ODN_Adafruit164Sensor implements ODN_DistanceSensor {
 
     private final AnalogInput m_input;
 
-    public ODN_AnalogUltrasonicSensor(int pin) {
+    public ODN_Adafruit164Sensor(int pin) {
         m_input = new AnalogInput(pin);
     }
 
     @Override
     public double getDistanceInches() {
-        double raw_value = m_input.getValue();
-
         double voltage_scale_factor = 5/RobotController.getVoltage5V();
+
+        double value = m_input.getValue() * voltage_scale_factor;
         
-        double currentDistanceInches = raw_value * voltage_scale_factor * 0.0492;
+        double currentDistanceInches = (168 * (1/value) - 4) / 13;
 
         return currentDistanceInches;
     }
