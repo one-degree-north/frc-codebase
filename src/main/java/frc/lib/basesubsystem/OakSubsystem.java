b8f5e5f06b996205438e17cd5ddc3f7401cc12d4
Vayun Mathur <69178052+vayun-mathur@users.noreply.github.com>
2022-02-16 19:46:56 +0800

    updated sds swerve lib and write data to oak

--- src/main/java/frc/lib/basesubsystem/OakSubsystem.java
+++ src/main/java/frc/lib/basesubsystem/OakSubsystem.java
@@ -3,6 +3,8 @@ package frc.lib.basesubsystem;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -15,25 +17,24 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class OakSubsystem extends SubsystemBase {
 
     public static class Detection {
-        String label;
-        int x1, y1, x2, y2;
-        double x, y, z;
+        public String label;
+        public int x1, y1, x2, y2;
+        public double x, y, z;
     }
 
     private BufferedReader m_read;
+    private PrintWriter m_write;
+    private Detection[] m_detections;
 
     public OakSubsystem(String hostname, int port) {
         Socket so;
         try {
             so = new Socket(hostname, port);
             m_read = new BufferedReader(new InputStreamReader(so.getInputStream()));
+            m_write = new PrintWriter(new OutputStreamWriter(so.getOutputStream()));
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public void evaluateDetections(Detection[] detections) {
-        
     }
 
     @Override
@@ -57,11 +58,19 @@ public class OakSubsystem extends SubsystemBase {
                     d.z = Double.parseDouble(arr[7]);
                     dec[i++] = d;
                 }
-                evaluateDetections(dec);
+                m_detections = dec;
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public Detection[] getDetections() {
+        return m_detections;
+    }
+
+    public void writeData(String s) {
+        m_write.write(s + "\n");
     }
     
 }
