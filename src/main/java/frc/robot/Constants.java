5c8aaa52e032c6837e835b9452f1c03349bc7bae
li42351 <60536150+li42351@users.noreply.github.com>
2022-02-28 07:43:15 +0800

    Changed acceleration

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -115,7 +115,7 @@ public final class Constants {
 
     public static final class AutoConstants {
         public static double kMaxSpeedMetersPerSecond = 1;
-        public static double kMaxAccelerationMetersPerSecondSquared = 1;
+        public static double kMaxAccelerationMetersPerSecondSquared = 0.8;
         
         public static final double kMaxAngularSpeedRadiansPerSecond = 40;
         public static final double kMaxAngularSpeedRadiansPerSecondSquared = 40;
