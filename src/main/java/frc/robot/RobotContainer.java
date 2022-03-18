3947829db44872b54cfaae1859a6b9c6e7805993
Vayun Mathur <69178052+vayun-mathur@users.noreply.github.com>
2022-03-18 22:51:17 +0800

    fixed intake reversing

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,45 +4,33 @@
 
 package frc.robot;
 
-import java.util.List;
-import java.util.Set;
-
-import javax.print.attribute.standard.Compression;
-
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.Compressor;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.lib.basesubsystem.LimelightSubsystem;
 import frc.lib.basesubsystem.SwerveDriveSubsystem;
 import frc.robot.commands.DriveCommand;
-import frc.robot.commands.ElevatorHeightCommand;
 import frc.robot.commands.IndexerContinueCommand;
-import frc.robot.commands.IndexerRunCommand;
 import frc.robot.commands.RotateCommand;
 import frc.robot.commands.ShootCommand;
-import frc.robot.commands.TrajectoryCommand;
 import frc.robot.commands.Wait;
 import frc.robot.subsystems.ClimbSubsystem;
 import frc.robot.subsystems.IndexerSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 
-
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
@@ -63,142 +51,162 @@ public class RobotContainer {
   // Robot commands go here:
   public Command getIntakeToggleCommand() {
     return new SequentialCommandGroup(
-    new InstantCommand(()->{
-      r = m_intake.isRunning();
-      if(r) {
-        m_intake.off();
-      } else {
-        m_intake.drop();
-      }
-    }),
-    new Wait(),
-    new InstantCommand(()->{
-      if(r) {
-        m_intake.raise();
-      } else {
-        m_intake.on();
-      }
-    })
-    
-  );
+        new InstantCommand(() -> {
+          r = m_intake.isRunning();
+          if (r) {
+            m_intake.off();
+          } else {
+            m_intake.drop();
+          }
+        }),
+        new Wait(),
+        new InstantCommand(() -> {
+          if (r) {
+            m_intake.raise();
+          } else {
+            m_intake.on();
+          }
+        })
+
+    );
   }
 
   // This command runs on autonomous
   private Command m_autoCommand = new SequentialCommandGroup(
-    new InstantCommand(()->m_climb.extendRotation(), m_climb),
-    new ShootCommand(m_shooter, m_indexer), 
-    new InstantCommand(()->m_climb.contractRotation(), m_climb),
-    new DriveCommand(m_drive, 90+14.5, 105),
-    new RotateCommand(m_drive, 180-14.5),
-    getIntakeToggleCommand(),
-    new DriveCommand(m_drive, 270, 22),
-    getIntakeToggleCommand(),
-    new RotateCommand(m_drive, -180+14.5),
-    new DriveCommand(m_drive, -90+14.5, 138),
-    new InstantCommand(()->m_climb.extendRotation(), m_climb),
-    new ShootCommand(m_shooter, m_indexer), 
-    new InstantCommand(()->m_climb.contractRotation(), m_climb),
-    new DriveCommand(m_drive, 90+14.5, 138)
+      new InstantCommand(() -> m_climb.extendRotation(), m_climb),
+      new ShootCommand(m_shooter, m_indexer),
+      new InstantCommand(() -> m_climb.contractRotation(), m_climb),
+      new DriveCommand(m_drive, 90 + 14.5, 105),
+      new RotateCommand(m_drive, 180 - 14.5),
+      getIntakeToggleCommand(),
+      new DriveCommand(m_drive, 270, 22),
+      getIntakeToggleCommand(),
+      new RotateCommand(m_drive, -180 + 14.5),
+      new DriveCommand(m_drive, -90 + 14.5, 138),
+      new InstantCommand(() -> m_climb.extendRotation(), m_climb),
+      new ShootCommand(m_shooter, m_indexer),
+      new InstantCommand(() -> m_climb.contractRotation(), m_climb),
+      new DriveCommand(m_drive, 90 + 14.5, 138)
 
-    );
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  );
+
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
     container = this;
 
     // Configure the button bindings
     configureButtonBindings();
 
-    // m_drive.setDefaultCommand(new RunCommand(() -> {
-      
-    //     m_drive.cartesianDriveRelative(modifyAxis(m_controller.getLeftY()), 
-    //       modifyAxis(m_controller.getLeftX()),
-    //       modifyAxis(m_controller.getRightX()));
-    //   },
-    //   m_drive));
+    m_drive.setDefaultCommand(new RunCommand(() -> {
 
-    m_indexer.setDefaultCommand(new RunCommand(()->{
-      //m_indexer.onboth();
-    }, m_indexer));
+    m_drive.cartesianDriveRelative(modifyAxis(m_controller.getLeftY()),
+    modifyAxis(m_controller.getLeftX()),
+    modifyAxis(m_controller.getRightX()));
+    },
+    m_drive));
 
-    //compressor.disable();
-    
     m_climb.disable();
     m_climb.contractRotation();
   }
 
   /**
-   * Use this method to define your button->command mappings. Buttons can be created by
+   * Use this method to define your button->command mappings. Buttons can be
+   * created by
    * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
+   * it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
     Trigger manualShooterRev = new JoystickButton(m_controller, XboxController.Button.kA.value);
     manualShooterRev.whenActive(() -> m_shooter.toggle(), m_shooter);
     /*
-    Trigger shootBall = new Trigger(()->m_controller.getRightTriggerAxis()>0.5);
-    Trigger intakeToggle3 = new Trigger(()->m_controller.getLeftTriggerAxis()>0.5);
-    shootBall.whenActive(new IndexerContinueCommand(m_indexer));
-    Trigger intakeToggle = new JoystickButton(m_controller, XboxController.Button.kRightBumper.value);
-    intakeToggle.whenActive(() -> m_indexer.onboth(), m_intake);
-    Trigger intakeToggle2 = new JoystickButton(m_controller, XboxController.Button.kLeftBumper.value);
-    intakeToggle2.whenActive(() -> m_indexer.off(), m_intake);
-    intakeToggle3.whenActive(() -> m_intake.toggleRun(), m_intake);
-`   */
-    
-    Trigger intakeRun = new Trigger(()->m_controller.getLeftTriggerAxis()>0.5);
-    Trigger shootBall = new Trigger(()->m_controller.getRightTriggerAxis()>0.5);
-    
+     * Trigger shootBall = new Trigger(()->m_controller.getRightTriggerAxis()>0.5);
+     * Trigger intakeToggle3 = new
+     * Trigger(()->m_controller.getLeftTriggerAxis()>0.5);
+     * shootBall.whenActive(new IndexerContinueCommand(m_indexer));
+     * Trigger intakeToggle = new JoystickButton(m_controller,
+     * XboxController.Button.kRightBumper.value);
+     * intakeToggle.whenActive(() -> m_indexer.onboth(), m_intake);
+     * Trigger intakeToggle2 = new JoystickButton(m_controller,
+     * XboxController.Button.kLeftBumper.value);
+     * intakeToggle2.whenActive(() -> m_indexer.off(), m_intake);
+     * intakeToggle3.whenActive(() -> m_intake.toggleRun(), m_intake);
+     * `
+     */
+
+    Trigger intakeRun = new Trigger(() -> m_controller.getLeftTriggerAxis() > 0.5);
+    Trigger shootBall = new Trigger(() -> m_controller.getRightTriggerAxis() > 0.5);
 
     Trigger t = new JoystickButton(m_controller, XboxController.Button.kB.value);
-    t.whenActive(()->{
-      if(compressor.enabled()) {
+    t.whenActive(() -> {
+      if (compressor.enabled()) {
         compressor.disable();
       } else {
         compressor.enableDigital();
       }
     });
-    
-    //drop or release
-    intakeRun.whenActive(getIntakeToggleCommand());
-    intakeRun.whenInactive(getIntakeToggleCommand());
 
-    m_indexer.ballAtExit().whileActiveOnce(new RunCommand(()->m_indexer.off(), m_indexer));
+    // drop or release
+    intakeRun.whenActive(new SequentialCommandGroup(
+        new InstantCommand(() -> {
+          m_intake.drop();
+        }),
+        new Wait(),
+        new InstantCommand(() -> {
+          m_intake.on();
+        })
 
-    //shoot ball
+    ));
+    intakeRun.whenInactive(new SequentialCommandGroup(
+        new InstantCommand(() -> {
+          m_intake.off();
+        }),
+        new Wait(),
+        new InstantCommand(() -> {
+          m_intake.raise();
+        })
+
+    ));
+
+    m_indexer.ballAtExit().whileActiveOnce(new RunCommand(() -> m_indexer.off(), m_indexer));
+
+    // shoot ball
     shootBall.whenActive(new IndexerContinueCommand(m_indexer));
 
-    //manual climber
-    Trigger climberUp = new  Trigger(()->m_controller.getPOV()==180);
-    Trigger climberDown = new  Trigger(()->m_controller.getPOV()==0);
+    // manual climber
+    Trigger climberUp = new Trigger(() -> m_controller.getPOV() == 180);
+    Trigger climberDown = new Trigger(() -> m_controller.getPOV() == 0);
     Trigger climberToggle = new JoystickButton(m_controller, XboxController.Button.kY.value);
     Trigger rotationToggle = new JoystickButton(m_controller, XboxController.Button.kX.value);
     double climbSpeed = 3000;
-    
-    climberToggle.whenActive(new InstantCommand(()-> m_climb.toggle()));
-    rotationToggle.whenActive(new InstantCommand(()-> m_climb.extendRotation()));
-    rotationToggle.whenInactive(new InstantCommand(()-> m_climb.contractRotation()));
-    climberUp.whileActiveContinuous(new InstantCommand(()-> m_climb.setMotor(climbSpeed), m_climb));
-    climberDown.whileActiveContinuous(new InstantCommand(()-> m_climb.setMotor(-climbSpeed), m_climb));
-    climberUp.whenInactive(new InstantCommand(()-> m_climb.setMotor(0), m_climb));
-    climberDown.whenInactive(new InstantCommand(()-> m_climb.setMotor(0), m_climb));
 
-    //climbing stuff: figure this out later
-    // JoystickButton climb = new JoystickButton(m_controller, XboxController.Button.kY.value);
+    climberToggle.whenActive(new InstantCommand(() -> m_climb.toggle()));
+    rotationToggle.whenActive(new InstantCommand(() -> m_climb.extendRotation()));
+    rotationToggle.whenInactive(new InstantCommand(() -> m_climb.contractRotation()));
+    climberUp.whileActiveContinuous(new InstantCommand(() -> m_climb.setMotor(climbSpeed), m_climb));
+    climberDown.whileActiveContinuous(new InstantCommand(() -> m_climb.setMotor(-climbSpeed), m_climb));
+    climberUp.whenInactive(new InstantCommand(() -> m_climb.setMotor(0), m_climb));
+    climberDown.whenInactive(new InstantCommand(() -> m_climb.setMotor(0), m_climb));
+
+    // climbing stuff: figure this out later
+    // JoystickButton climb = new JoystickButton(m_controller,
+    // XboxController.Button.kY.value);
     // climb.whenPressed(new SequentialCommandGroup(
-    //   new InstantCommand(()->m_climb.enable(), m_climb),
-    //   new ElevatorHeightCommand(m_climb, ClimbSubsystem.TOP),
-    //   new DriveBackCommand(m_drive),
-    //   new ElevatorHeightCommand(m_climb, ClimbSubsystem.BOTTOM),
-    //   new InstantCommand(()->m_climb.contractRotation(), m_climb),
-    //   new ElevatorHeightCommand(m_climb, ClimbSubsystem.TRANSFER),
-    //   new InstantCommand(()->m_climb.extendRotation(), m_climb),
-    //   new ElevatorHeightCommand(m_climb, ClimbSubsystem.TOP)
+    // new InstantCommand(()->m_climb.enable(), m_climb),
+    // new ElevatorHeightCommand(m_climb, ClimbSubsystem.TOP),
+    // new DriveBackCommand(m_drive),
+    // new ElevatorHeightCommand(m_climb, ClimbSubsystem.BOTTOM),
+    // new InstantCommand(()->m_climb.contractRotation(), m_climb),
+    // new ElevatorHeightCommand(m_climb, ClimbSubsystem.TRANSFER),
+    // new InstantCommand(()->m_climb.extendRotation(), m_climb),
+    // new ElevatorHeightCommand(m_climb, ClimbSubsystem.TOP)
     // ));
-    
+
   }
-  
-  
+
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
