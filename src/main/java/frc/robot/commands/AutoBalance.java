b7bf2dd1c068255da4affdc1e329838d6fb5b8cb
marcmchin <32217062+marcmchin@users.noreply.github.com>
2023-01-09 13:30:26 +0800

    autobalance fixes

--- src/main/java/frc/robot/commands/AutoBalance.java
+++ src/main/java/frc/robot/commands/AutoBalance.java
@@ -15,7 +15,6 @@ public class AutoBalance extends CommandBase {
   Rotation2d m_initpitch;
   double m_deadband;
   double m_speed;
-  boolean balanceMode = false;
 
   /** Creates a new AutoBalance. */
   public AutoBalance(ODN_AHRS navx, SwerveDriveSubsystem drive, Rotation2d initpitch, double deadband, double speed) {
@@ -32,34 +31,23 @@ public class AutoBalance extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    balanceMode = false;
     m_drive.cartesianDriveRelative(0, 0, 0);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if (m_navx.getPitch().getDegrees() >= m_initpitch.getDegrees() + m_deadband || 
-    m_navx.getPitch().getDegrees() <= m_initpitch.getDegrees() - m_deadband) balanceMode = true;
-
-    else balanceMode = false;
-
-    if (balanceMode) {
-      if (m_navx.getPitch().getDegrees() >= m_initpitch.getDegrees() + m_deadband)
-        m_drive.cartesianDriveRelative(-m_speed, 0, 0);
-      if (m_navx.getPitch().getDegrees() <= m_initpitch.getDegrees() - m_deadband)
-        m_drive.cartesianDriveRelative(m_speed, 0, 0);
-    }
-
-    if (!balanceMode) {
+    if (m_navx.getPitch().getDegrees() >= m_initpitch.getDegrees() + m_deadband)
+      m_drive.cartesianDriveRelative(-m_speed, 0, 0);
+    else if (m_navx.getPitch().getDegrees() <= m_initpitch.getDegrees() - m_deadband)
+      m_drive.cartesianDriveRelative(m_speed, 0, 0);
+    else
       m_drive.cartesianDriveAbsolute(0, 0, 0);
-    }
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    balanceMode = false;
     m_drive.cartesianDriveAbsolute(0, 0, 0);
   }
 
